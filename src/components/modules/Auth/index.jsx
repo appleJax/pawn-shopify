780ed58ad3659a6b89cd10973a77074660c3e7a9
smakosh <ismai23l@hotmail.com>
2018-11-03 23:08:37 +0000

    debug auth0

--- src/components/modules/Auth/index.jsx
+++ src/components/modules/Auth/index.jsx
@@ -1,32 +1,32 @@
-// import React from 'react'
-// import { AuthProvider, AuthConsumer, Button } from 'Common'
+import React from 'react'
+import { AuthProvider, AuthConsumer, Button } from 'Common'
 
-// const Auth = ({ title }) => (
-// 	<AuthProvider>
-// 		<AuthConsumer>
-// 			{({ isAuth, login, logout, user }) => {
-// 				const Login = (
-// 					<Button as="a" href="#" onClick={login}>
-// 						{title}
-// 					</Button>
-// 				)
-// 				const Logout = (
-// 					<Button
-// 						as="a"
-// 						href="#"
-// 						onClick={e => {
-// 							e.preventDefault()
-// 							logout()
-// 						}}
-// 					>
-// 						Log Out
-// 						{user && <span>{user.name}</span>}
-// 					</Button>
-// 				)
-// 				return isAuth ? Logout : Login
-// 			}}
-// 		</AuthConsumer>
-// 	</AuthProvider>
-// )
+const Auth = ({ title }) => (
+ 	<AuthProvider>
+ 		<AuthConsumer>
+ 			{({ isAuth, login, logout, user }) => {
+ 				const Login = (
+ 					<Button as="a" href="#" onClick={login}>
+ 						{title}
+ 					</Button>
+ 				)
+ 				const Logout = (
+ 					<Button
+ 						as="a"
+ 						href="#"
+ 						onClick={e => {
+ 							e.preventDefault()
+ 							logout()
+ 						}}
+ 					>
+ 						Log Out
+ 						{user && <span>{user.name}</span>}
+ 					</Button>
+ 				)
+ 				return isAuth ? Logout : Login
+ 			}}
+ 		</AuthConsumer>
+ 	</AuthProvider>
+)
 
-// export default Auth
+export default Auth

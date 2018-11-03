eacee1546ef744fcfc19ce7d823e868a5807facf
smakosh <ismai23l@hotmail.com>
2018-11-03 20:54:10 +0000

    uncomment auth

--- src/components/theme/HomeContent/index.jsx
+++ src/components/theme/HomeContent/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Container } from 'Common'
 import Undraw from '../../../../static/illustrations/sell.svg'
-// import Auth from '../../modules/Auth'
+import Auth from '../../modules/Auth'
 import { Wrapper, Content } from './styles'
 
 const HomeContent = () => (
@@ -12,7 +12,7 @@ const HomeContent = () => (
                 to quickly start selling
                 & <span>enjoy</span> the profit!
 			</h2>
-			{/* <Auth title="Create account" /> */}
+			<Auth title="Create account" />
 		</Content>
 		<Content>
 			<img src={Undraw} alt="classify, sell & have fun" />

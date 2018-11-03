91924d970ba7b3c44e2ff0fc5f120b3358dea291
jarrodyellets <jarrod524@gmail.com>
2018-11-03 23:06:16 +0100

    Username from email

--- src/components/theme/Profile/index.jsx
+++ src/components/theme/Profile/index.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import { AuthConsumer, AuthProvider } from 'Common'
+import { UserName, UserWrapper } from './styles'
 
 const Profile = () => (
 	<AuthProvider>
 		<AuthConsumer>
 			{({ isAuth, user }) => {
 				const LoggedIn = (
-
-					<h1>Welcome {user && <span>{user.name}</span>}</h1>
+					<UserWrapper>
+						<UserName>Welcome {user && <span>{user.name.substring(0, user.name.indexOf('@'))}</span>}</UserName>
+					</UserWrapper>
 
 				)
 				return isAuth ? LoggedIn : null
---
+++ src/components/theme/Profile/styles.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+export const UserName = styled.div`
+  font-size: 1.3em;
+`
+
+export const UserWrapper = styled.div`
+  max-width: 1280px;
+  width: 70%;
+  margin: 0 auto;
+`

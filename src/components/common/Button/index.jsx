b819bf030501d20a88b11d9c522ae8c510114d01
appleJax <appleJax@users.noreply.github.com>
2018-11-03 23:52:58 -0500

    add margin to buttons

--- src/components/common/Button/index.jsx
+++ src/components/common/Button/index.jsx
@@ -4,6 +4,7 @@ export const Button = styled.button`
     background: #5C6AC4;
     border: none;
     box-shadow: 0 4px 6px 0 rgba(0, 0, 0, 0.2);
+    margin: 5px;
     padding: .6rem 1.5rem;
     color: #fff;
     border-radius: 6px;
